f5ec9b434e33a1cd7e0bd7ef950acf501be0d904
Marco Scholl <develop@marco-scholl.de>
2010-12-08 10:35:35 +0100

    validator class

---
+++ lib/rails_email_validator.rb
@@ -0,0 +1,39 @@
+require 'active_model'
+
+class EmailValidator < ActiveModel::EachValidator
+  # check the options for do mx validation
+  def validate_mx?
+    options[:validate_mx].nil? or options[:validate_mx] == true
+  end
+
+  # validate if an mx exists on domain
+  def has_mx?(domain)
+    require 'resolv'
+    mx = []
+    Resolv::DNS.open do |dns|
+      mx = dns.getresources(domain, Resolv::DNS::Resource::IN::MX)
+    end
+    mx.size > 0
+  end
+
+  # main validator for email
+  def validate_each(record, attribute, value)
+    # split local and domain part
+    (local_part, domain_part) = value.split('@', 2)
+
+    # pre var
+    valid = true
+
+    # check syntax
+    valid = false unless local_part =~ /\A[A-Za-z0-9.!\#$%&'*+-\/=?^_`{|}~]+\Z/
+    valid = false unless domain_part =~ /\A((?:[-a-zA-Z0-9]+\.)+[a-zA-Z]{2,})\Z/
+
+    # check mx
+    if valid and validate_mx?
+      valid = false unless has_mx? domain_part
+    end
+
+    # email valid
+    record.errors[attribute] << :invalid unless valid
+  end
+end

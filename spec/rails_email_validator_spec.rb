6a277443a3941dbb851700d509f23180cfd6dba1
Marco Scholl <develop@marco-scholl.de>
2011-02-13 18:13:07 +0100

    cleanup

--- spec/rails_email_validator_spec.rb
+++ spec/rails_email_validator_spec.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "rails email validator" do
+describe EmailValidator do
   before do
     class ValidateEmail
       include ActiveModel::Validations
